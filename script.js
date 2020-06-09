d8b2e585fcdb1e5b637ac3f853d6f64c00d92282
flucasjs <flucasjs@gmail.com>
2020-06-08 19:56:31 -0700

    refactored theme toggle

--- script.js
+++ script.js
@@ -8,28 +8,30 @@ const additem = document.querySelector(".add-item");
 const header = document.querySelector(".header");
 const container = document.querySelector(".container");
 
+// Arguements for date text.
+const options = {weekday: "long", month: "short", day: "numeric"};
+const today = new Date();
+
+// Todays date.
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 // Visual elements used to indicate completion of items.
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle";
 const LINE_THROUGH = "lineThrough";
 
-
 // Visual element used to toggle theme settings.
 const TOGGLEON = "fa-toggle-on";
 const TOGGLEOFF = "fa-toggle-off";
 
-
 // Variables used for local storage.
 let LIST, id;
-
 
 // Retrieve to do list from local storage.
 let data = localStorage.getItem("TODO");
 
 // Retrieve theme setting from local storage.
 let style = localStorage.getItem("THEME");
-
 
 // Data loader. Creates new local storage list if data is empty.
 if (data) {
@@ -50,25 +52,9 @@ if (style == "dark") {
     
     let element = document.getElementById("toggle");
 
-    element.classList.toggle(TOGGLEON);
-    element.classList.toggle(TOGGLEOFF);
-
-    document.body.style.background = "rgba(0, 0, 0, 0.75)";
-    content.style.background = "lightgray";
-    additem.style.background = "lightgray";
-    header.style.backgroundImage = "url('img2.svg')";
-    header.style.backgroundSize = "150%";
-    header.style.backgroundPosition = "initial";
+    setTheme(toggle);
 
 }
-
-// Load list retrieved from local storage.
-function loadList(array) {
-
-    array.forEach( (item) => addToDo(item.name, item.id, item.done, item.trash) );
-
-}
-
 
 // Clear and reload the page when the user clicks the clear icon in the header area.
 clear.addEventListener("click", () => {
@@ -77,40 +63,6 @@ clear.addEventListener("click", () => {
     location.reload();
 
 });
-
-
-// Arguements for date text.
-const options = {weekday: "long", month: "short", day: "numeric"};
-const today = new Date();
-
-
-// Todays date.
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
-
-
-// Appends a to do item to the list located in the content area.
-function addToDo(toDo, id, done, trash) {
-    
-    if (trash) {
-
-        return;
-
-    }
-
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
-
-    const item = `<li class="item">
-                    <i class="far ${DONE}" data-job="complete" id="${id}"></i>
-                    <p class="text ${LINE}">${toDo}</p>
-                    <i class="far fa-trash-alt" data-job="delete" id="${id}"></i>
-                  </li>`;
-
-    const position = "beforeend";
-    
-    list.insertAdjacentHTML(position, item);
-
-}
 
 
 // Add a new item to the list when the user hits the enter key.
@@ -145,6 +97,35 @@ document.addEventListener("keyup", (event) => {
 
 });
 
+// Complete or delete an item depending on which icon the user clicks.
+list.addEventListener("click", (event) => {
+
+    const element = event.target;
+    const elementJob = element.dataset.job;
+
+    if (elementJob == "complete") {
+
+        completeToDo(element);
+
+    } else if (elementJob == "delete") {
+
+        removeToDo(element);
+
+    }
+
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+
+});
+
+// Dark and light theme toggle
+theme.addEventListener("click", (event) => {
+
+    const element = event.target;
+    
+    setTheme(element);
+
+});
 
 // Apply visual elements when a user has finished or restarted an item in the list.
 // Update the list item to reflect the state of the item.
@@ -169,49 +150,57 @@ function removeToDo(element) {
 
 }
 
+// Appends a to do item to the list located in the content area.
+function addToDo(toDo, id, done, trash) {
+    
+    if (trash) {
 
-// Complete or delete an item depending on which icon the user clicks.
-list.addEventListener("click", (event) => {
-
-    const element = event.target;
-    const elementJob = element.dataset.job;
-
-    if (elementJob == "complete") {
-
-        completeToDo(element);
-
-    } else if (elementJob == "delete") {
-
-        removeToDo(element);
+        return;
 
     }
 
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
 
-    localStorage.setItem("TODO", JSON.stringify(LIST));
+    const item = `<li class="item">
+                    <i class="far ${DONE}" data-job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="far fa-trash-alt" data-job="delete" id="${id}"></i>
+                  </li>`;
 
-});
-
-
-function changeBodyColor(color) {
-
-    document.body.style.background = color;
+    const position = "beforeend";
+    
+    list.insertAdjacentHTML(position, item);
 
 }
 
+// Load list retrieved from local storage.
+function loadList(array) {
 
-// Dark and light theme toggle
-theme.addEventListener("click", (event) => {
+    array.forEach( (item) => addToDo(item.name, item.id, item.done, item.trash) );
 
-    const element = event.target;
-    
+}
+
+// Toggle between light and dark themes.
+function setTheme(element) {
+
     element.classList.toggle(TOGGLEON);
     element.classList.toggle(TOGGLEOFF);
 
+    let darkBackgroundColor = "rgba(0, 0, 0, .75)";
+    let darkContentColor = "lightgray";
+    let darkItemColor = "lightgray";
+
+    let lightBackgroundColor = "rgb(137, 201, 170)";
+    let lightContentColor = "white";
+    let lightItemColor = "white";
+
     if (element.classList.contains(TOGGLEON)) {
 
-        document.body.style.background = "rgba(0, 0, 0, 0.75)";
-        content.style.background = "lightgray";
-        additem.style.background = "lightgray";
+        document.body.style.background = darkBackgroundColor;
+        content.style.background = darkContentColor;
+        additem.style.background = darkItemColor;
+
         header.style.backgroundImage = "url('img2.svg')";
         header.style.backgroundSize = "150%";
         header.style.backgroundPosition = "initial";
@@ -221,9 +210,10 @@ theme.addEventListener("click", (event) => {
         
     } else {
 
-        document.body.style.background = "rgb(137, 201, 170)";
-        content.style.background = "white";
-        additem.style.background = "white";
+        document.body.style.background = lightBackgroundColor;
+        content.style.background = lightContentColor ;
+        additem.style.background = lightItemColor;
+
         header.style.backgroundImage = "url('img1.svg')";
         header.style.backgroundSize = "200%";
         header.style.backgroundPosition = "-100px -175px";
@@ -231,25 +221,4 @@ theme.addEventListener("click", (event) => {
         localStorage.setItem("THEME", "light");
 
     }
-
-});
-
-
-// Loads the appropriate theme upon location reload.
-window.addEventListener("load", () => {
-
-    if (style == "dark") {
-
-        header.style.backgroundImage = "url('img2.svg')";
-        header.style.backgroundSize = "150%";
-        header.style.backgroundPosition = "initial";
-        
-    } else {
-
-        header.style.backgroundImage = "url('img1.svg')";
-        header.style.backgroundSize = "200%";
-        backgroundPosition = "-100px -175px";
-        
-    }
-    
-});
+}
